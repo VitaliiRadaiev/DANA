b43cfeea56fdce837140b6fc5199a9d486fb716c
Spacespire <aleksandra@spacespire.com>
2022-05-10 11:58:26 +0300

    fixes done

--- dist/js/app.js
+++ dist/js/app.js
@@ -316,16 +316,21 @@ if (header) {
 
     if(menu) {
         let burger = document.querySelector('[data-action="open-menu"]');
-        let closeBtn = document.querySelector('[data-action="close-menu"]');
+        let closeBtns = document.querySelectorAll('[data-action="close-menu"]');
 
         burger.addEventListener('click', () => {
             menu.classList.add('menu--open');
             document.body.classList.add('overflow-hidden');
         })
-        closeBtn.addEventListener('click', () => {
-            menu.classList.remove('menu--open');
-            document.body.classList.remove('overflow-hidden');
-        })
+        if(closeBtns.length) {
+            closeBtns.forEach(closeBtn => {
+                closeBtn.addEventListener('click', () => {
+                    menu.classList.remove('menu--open');
+                    document.body.classList.remove('overflow-hidden');
+                })
+            })
+        }
+
         const setHeight = () => {
             if(document.documentElement.clientWidth < 992) {
                 menu.style.height = document.documentElement.clientHeight + 'px';
